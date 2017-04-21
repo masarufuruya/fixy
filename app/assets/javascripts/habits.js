d03ea0654cd08846c2f9721bf5410fce760942ec
古家大 <yannwennri2@gmail.com>
2017-04-21 21:47:03 +0900

    change memo order

--- app/assets/javascripts/habits.js
+++ app/assets/javascripts/habits.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
       var $memoList = $('.memo__list');
       $memoList.empty();
       $.each(data.memos, function(i, memo) {
-        var createdAt = moment(memo.created_at).format("MM月DD日 H:m:s");
+        var createdAt = moment(memo.created_at).format("MM月DD日 H:mm:ss");
         $memoList.append('<p style="font-size:12px;color:#888;margin-bottom:0;">'+createdAt+'</p>');
         $memoList.append('<li style="list-style:none;font-size:14px;padding: 10px 0;">'+commonJs.br(memo.body)+'</li>');
       });
